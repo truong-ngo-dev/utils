56c9fb82f5d99341f9eba031d7540245c4fe4631
truong.ngo.xuan.dev <truong.ngo.xuan.dev@gmail.com>
2025-03-29 11:30:58 +0700

    add

--- src/main/java/com/nob/utils/JsonUtils.java
+++ src/main/java/com/nob/utils/JsonUtils.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
@@ -11,6 +13,8 @@ import java.util.Objects;
  * Json utility
  * */
 public class JsonUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonUtils.class);
 
     /**
      * Default object mapper
@@ -25,36 +29,56 @@ public class JsonUtils {
 
 
     /**
-     * Convert json string to object type {@code T}
-     * @param json json string
-     * @param clazz class type of {@code T}
-     * @param <T> object type
-     * @throws JsonProcessingException if json string is invalid
-     * */
-    public static <T> T fromJson(String json, Class<T> clazz) throws JsonProcessingException {
-        return Objects.isNull(json) ? null : MAPPER.readValue(json, clazz);
+     * Converts a JSON string into an object of the specified type {@code T}.
+     *
+     * @param json  The JSON string to be converted.
+     * @param clazz The class type of {@code T}.
+     * @param <T>   The target object type.
+     * @return An object of type {@code T} parsed from the JSON string, or {@code null} if the input is {@code null}.
+     * @throws IllegalArgumentException If the JSON string is invalid or cannot be deserialized.
+     */
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return Objects.isNull(json) ? null : MAPPER.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to parse json string: {}, cause: {}", json, e.getMessage(), e);
+            throw new IllegalArgumentException("Failed to parse json string: " + json + ", cause: " + e.getMessage(), e);
+        }
     }
 
 
     /**
-     * Convert json string to object type {@code T}
-     * @param json json string
-     * @param type parameterize type of {@code T}
-     * @param <T> object type
-     * @throws JsonProcessingException if json string is invalid
-     * */
-    public static <T> T fromJson(String json, TypeReference<T> type) throws JsonProcessingException {
-        return Objects.isNull(json) ? null : MAPPER.readValue(json, type);
+     * Converts a JSON string into an object of the specified parameterized type {@code T}.
+     *
+     * @param json The JSON string to be converted.
+     * @param type The parameterized type reference of {@code T}.
+     * @param <T>  The target object type.
+     * @return An object of type {@code T} parsed from the JSON string, or {@code null} if the input is {@code null}.
+     * @throws IllegalArgumentException If the JSON string is invalid or cannot be deserialized.
+     */
+    public static <T> T fromJson(String json, TypeReference<T> type) {
+        try {
+            return Objects.isNull(json) ? null : MAPPER.readValue(json, type);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to parse json string: {}, cause: {}", json, e.getMessage(), e);
+            throw new IllegalArgumentException("Failed to parse json string: " + json + ", cause: " + e.getMessage(), e);
+        }
     }
 
 
     /**
-     * Serialize object to json string
-     * @param obj object
-     * @return serialize json string
-     * @throws JsonProcessingException if error occur when process json serialization
-     * */
-    public static String toJson(Object obj) throws JsonProcessingException {
-        return MAPPER.writeValueAsString(obj);
+     * Serializes an object into a JSON string.
+     *
+     * @param obj The object to be serialized.
+     * @return A JSON string representation of the object.
+     * @throws IllegalArgumentException If serialization fails.
+     */
+    public static String toJson(Object obj) {
+        try {
+            return MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to stringify json object: {}", e.getMessage(), e);
+            throw new IllegalArgumentException("Failed to stringify json object: " + e.getMessage(), e);
+        }
     }
 }
